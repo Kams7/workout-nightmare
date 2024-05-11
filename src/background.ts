e0b1b1ed4053b4d36392ec65c791577523b061fc
Kams7 <kamaleshman7@gmail.com>
2024-05-11 23:00:00 +0530

    remind-time-feature

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,17 @@
 let safe = false
 let sufferUrl = chrome.runtime.getURL("tabs/suffer.html")
+let sufferTime = { hours: 0, minutes: 0 };
 
 function initializeVariables(callback) {
-  chrome.storage.local.get(["lastDate", "isSafe"], function (result) {
+  chrome.storage.local.get(["lastDate", "isSafe", "sufferTime"], function (result) {
+    sufferTime = result.sufferTime || { hours: 0, minutes: 0 };
     const full_today = new Date();
-    full_today.setHours(full_today.getHours() - 17);
+    const { hours, minutes } = sufferTime;
+    full_today.setHours(full_today.getHours() - hours);
+    full_today.setMinutes(full_today.getMinutes() - minutes);
 
     const today = full_today.toLocaleDateString();
-
+    // console.log(full_today, today);
 
     if (result.lastDate !== today) {
       // Update the date in storage
@@ -32,54 +36,73 @@ initializeVariables(function() {
     }
   });
 
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === "changeSufferTime") {
+      sufferTime = message.value;
+      console.log("sufferTime updated to:", sufferTime);
+      chrome.storage.local.set({ "sufferTime": sufferTime });
 
+      // Update the date in storage
+      const full_today = new Date();
+      const { hours, minutes } = sufferTime;
+      full_today.setHours(full_today.getHours() - hours);
+      full_today.setMinutes(full_today.getMinutes() - minutes);
+  
+      const today = full_today.toLocaleDateString();
+      chrome.storage.local.set({ "lastDate": today });
 
-
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-    if (changeInfo.status === "loading" && safe === false && tab.url !== sufferUrl) {
-      chrome.tabs.query({ url: sufferUrl }, function (sufferTabs) {
-        if (sufferTabs && sufferTabs.length > 0) {
-          chrome.tabs.update(sufferTabs[0].id, { active: true });
-        } else {
-          // If "suffer" tab is not open, create a new one
-          chrome.tabs.create({ url: sufferUrl });
-        }
-      });
     }
-
-
   });
+
+
+  // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+  //   if (changeInfo.status === "loading" && safe === false && tab.url !== sufferUrl) {
+  //     chrome.tabs.query({ url: sufferUrl }, function (sufferTabs) {
+  //       if (sufferTabs && sufferTabs.length > 0) {
+  //         chrome.tabs.update(sufferTabs[0].id, { active: true });
+  //       } else {
+  //         // If "suffer" tab is not open, create a new one
+  //         chrome.tabs.create({ url: sufferUrl });
+  //       }
+  //     });
+  //   }
+
+
+  // });
 
 
 
   chrome.tabs.onActivated.addListener(function(activeInfo) {
     // console.log("switched");
-    if (safe==false){
+    initializeVariables(function(){
+      if (safe==false){
 
 
-      let repeat;
-      repeat = setInterval(function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          if (tabs[0].url !== sufferUrl && !safe) {
-              setTimeout(() => {
-                if (safe || tabs[0].url === sufferUrl) clearInterval(repeat)
-                chrome.tabs.query({ url: sufferUrl }, function (sufferTabs) {
-                  if (sufferTabs && sufferTabs.length > 0) {
-                    chrome.tabs.update(sufferTabs[0].id, { active: true });
-                  } else {
-                    // If "suffer" tab is not open, create a new one
-                    chrome.tabs.create({ url: sufferUrl });
-                  }
-                });
-              }, 100)
-          }
-        });
-      }, 100);
+        let repeat;
+        repeat = setInterval(function(){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            if (tabs && tabs.length>0 && tabs[0].url !== sufferUrl && !safe) {
+                setTimeout(() => {
+                  if (safe || tabs[0].url === sufferUrl) clearInterval(repeat)
+                  chrome.tabs.query({ url: sufferUrl }, function (sufferTabs) {
+                    if (sufferTabs && sufferTabs.length > 0) {
+                      chrome.tabs.update(sufferTabs[0].id, { active: true });
+                    } else {
+                      // If "suffer" tab is not open, create a new one
+                      chrome.tabs.create({ url: sufferUrl });
+                    }
+                  });
+                }, 100)
+            }
+          });
+        }, 100);
+  
+  
+  
+      }
+    });
 
-
-
-    }
   });
 
 
--- src/popup.tsx
+++ src/popup.tsx
@@ -4,7 +4,7 @@ import "./../style.css"
 
 function IndexPopup() {
   const [data, setData] = useState("")
-  const [count, setCount] = useState(0);
+  const [time, setTime] = useState("00:00");
 
   const handleWorkout = () => {
     chrome.tabs.create({
@@ -12,8 +12,23 @@ function IndexPopup() {
     })
   }
 
+  useEffect(() => {
+    // Fetch the initial value of sufferTime from storage and set it in the state
+    chrome.storage.local.get(["sufferTime"], function(result) {
+      if (result.sufferTime !== undefined) {
+        setTime(`${String(result.sufferTime.hours).padStart(2, '0')}:${String(result.sufferTime.minutes).padStart(2, '0')}`);
+      }
+      // console.log(time);
+    });
+  }, []);
+
   const changeSafe = () => {
     chrome.runtime.sendMessage({ action: "changeSafe", value: false });
+  }
+
+  const changeSufferTime = () => {
+    const [hours, minutes] = time.split(":");
+    chrome.runtime.sendMessage({ action: "changeSufferTime", value: { hours: parseInt(hours), minutes: parseInt(minutes) } });
   }
 
   return (
@@ -27,6 +42,14 @@ function IndexPopup() {
       </button>
       <button className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2"
        onClick={changeSafe}>Reset Day</button>
+
+      <h2 className="mt-4">
+        Change time for workout
+      </h2>
+      <div className="flex items-center">
+        <input type="time" value={time} onChange={(e) => {setTime(e.target.value)}} className="mt-2 w-full px-3 py-2 text-base text-white placeholder-gray-400 bg-[#24292F] rounded-lg focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 focus:ring-opacity-50" />
+        <button onClick={changeSufferTime} className="mt-2 px-4 py-2 ml-2 bg-[#4B5563] text-white hover:bg-[#4B5563]/90 focus:outline-none focus:ring-2 focus:ring-[#4B5563] rounded-lg">Set</button>
+      </div>
     </div>
   )
 }
